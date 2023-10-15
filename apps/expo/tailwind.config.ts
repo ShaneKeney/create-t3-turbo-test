3ca938643f5275078aecd38a476426ac8b54f31e
Shane Keney <shane.keney@curaleaf.com>
2023-10-15 10:31:43 -0400

    test: use rally2 config options and eas profiles

--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -1,8 +1,23 @@
-import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
 
-import baseConfig from "@acme/tailwind-config";
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  presets: [require("@acme/tailwind-config")],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Works as expected
+        // e.g. bg-brandPrimary vs. dark:bg-brandPrimaryDark
+        brandPrimary: "#1DE083",
 
-export default {
-  content: ["./src/**/*.{ts,tsx}"],
-  presets: [baseConfig],
-} satisfies Config;
+        background: colors.gray[100],
+        backgroundDark: colors.slate[900],
+      },
+    },
+  },
+};
